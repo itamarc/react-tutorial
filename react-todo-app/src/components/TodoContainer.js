7af5942041292199e5cd9994464b1f1816392375
Itamar Carvalho <itamarc@gmail.com>
2021-09-14 17:46:50 -0300

    Tutorial part 4 - editing

--- react-todo-app/src/components/TodoContainer.js
+++ react-todo-app/src/components/TodoContainer.js
@@ -56,6 +56,16 @@ class TodoContainer extends React.Component {
             todos: [...this.state.todos, newTodo]
         });
     };
+    setUpdate = (updatedTitle, id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.title = updatedTitle
+                }
+                return todo
+            }),
+        })
+    };
     render() {
         return (
             <div className="container">
@@ -64,7 +74,8 @@ class TodoContainer extends React.Component {
                     <InputTodo addTodoProps={this.addTodoItem} />
                     <TodosList todos={this.state.todos}
                         handleChangeProps={this.handleChange}
-                        deleteTodoProps={this.delTodo} />
+                        deleteTodoProps={this.delTodo}
+                        setUpdate={this.setUpdate} />
                 </div>
             </div>
         );
